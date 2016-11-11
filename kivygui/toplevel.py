7c93ea26c8d7aaeb3545a6078ff268498099a8a1
timeyyy <timeyyy_da_man@hotmail.com>
2016-11-11 17:02:54 +0100

    keybind typo fixed.. damn u dynamic languages

--- kivygui/toplevel.py
+++ kivygui/toplevel.py
@@ -8,6 +8,7 @@
 import logging
 import queue
 import json
+import os
 from contextlib import suppress
 
 from system_hotkey import SystemHotkey
@@ -15,6 +16,7 @@ import peasoup
 
 from kivy.app import App
 from kivy.base import KivyEventLoop
+from kivy.lang import Builder
 from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -22,7 +24,7 @@ from kivy.uix.screenmanager import FadeTransition
 
 from kivygui.presenter import Requests
 from kivygui.presenter import Responses
-# Have to import all the components used for kv lang to use
+# Have to import all the components used for kv files to use
 from kivygui.editor import UncrumpledEditor
 from kivygui.cmdpane import CommandPane
 from kivygui.workbench import Workbench
@@ -33,11 +35,11 @@ from kivygui.rules import Style
 
 
 class Keybinder():
-    def setup_config(self):
-        self._keyboard = self.window.request_keyboard(self._keyboard_closed, self)
+    def setup_keybind(self, window):
+        self._keyboard = window.request_keyboard(self._keyboard_closed, self)
 
     def _keyboard_closed(self):
-        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self._keyboard.unbind(on_key_down=self.handler_on_key_down)
         self._keyboard = None
 
     def _run_bind(self, callback_string):
@@ -56,7 +58,6 @@ class Keybinder():
         return json.dumps(hotkey)
 
     def handler_on_key_down(self, _, keycode, keysym, modifiers):
-        # import pdb;pdb.set_trace()
         hotkey = self._make_hkstring(keysym, modifiers)
         commands = self.active_binds['on_key_down'].get(hotkey)
         with suppress(TypeError):
@@ -74,6 +75,7 @@ class UncrumpledWindow(Screen, Style, Responses, Requests, Keybinder):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.window = Window
+        self.setup_keybind(self.window)
         self.hk = SystemHotkey(consumer=self.sys_hotkey_handler,
                                check_queue_interval=0.001)
         self.hk.register(['f7'], self.req_system_get) # JFT
@@ -110,16 +112,16 @@ class UncrumpledWindow(Screen, Style, Responses, Requests, Keybinder):
             self.run_in_main(lambda: self.req_hotkey_pressed(
                                         profile, program, hotkey))
 
-    def touch_handler(self, _, touch): # JFT
-        if self.ids.commandpane.collide_point(*touch.pos):
-            self.ids.commandpane.toggle()
+    # def touch_handler(self, _, touch): # JFT
+        # if self.ids.commandpane.collide_point(*touch.pos):
+            # self.ids.commandpane.toggle()
             # touch.grab(self.ids.commandpane)
-            return True
-        if self.ids.workbench.collide_point(*touch.pos):
-            self.ids.workbench.toggle()
-            return True
-        self.ids.workbench.visible = 1
-        self.ids.commandpane.visible = 1
+            # return True
+        # if self.ids.workbench.collide_point(*touch.pos):
+            # self.ids.workbench.toggle()
+            # return True
+        # self.ids.workbench.visible = 1
+        # self.ids.commandpane.visible = 1
 
 
 class ToplevelApp(App):
@@ -127,25 +129,42 @@ class ToplevelApp(App):
         '''call to start the gui (only used by the backend)'''
         self.unc_app = unc_app
         self.ev = KivyEventLoop(self)
-        # self.ev.set_debug(True)
+        if self.unc_app.DEVELOPING:
+            # Has some performance penalties
+            self.ev.set_debug(True)
         self.ev.mainloop()
 
     def build(self):
         root = MyScreenManager()
         Clock.schedule_once(lambda e: root.remove_loadscreen(), 4)
-        # If we are are being run by the backend
-        if hasattr(self, 'ev'):
-            # Find the widget handling our requests/responses and call ui_init
-            for screen in root.screens:
-                if screen.name == 'uncrumpled':
-                    screen.ev = self.ev
-                    screen._unc_app = self.unc_app
-                    # screen.kivy_app = self
-                    Clock.schedule_once(lambda e: screen.setup_config(), 0)
-                    screen.req_ui_init() # TODO ASYNC THIS
-                    break
+        # Find the widget handling our requests/responses and call ui_init
+        for screen in root.screens:
+            if screen.name == 'uncrumpled':
+                screen.ev = self.ev
+                screen._unc_app = self.unc_app
+                # screen.kivy_app = self
+                screen.req_ui_init() # TODO ASYNC THIS
+                break
         return root
 
 
 if __name__ == '__main__':
-    ToplevelApp().run()
+
+    class TestSync(ToplevelApp):
+        def build(self):
+            path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'toplevel.kv'))
+            # Required becaues this class name != name of kv file
+            Builder.load_file(path)
+            root = MyScreenManager()
+            Clock.schedule_once(lambda e: root.remove_loadscreen(), 4)
+            return root
+
+    class TestAsync(TestSync):
+        def start(self):
+            logging.basicConfig(level=logging.DEBUG)
+            self.ev = KivyEventLoop(self)
+            self.ev.set_debug(True)
+            self.ev.mainloop()
+
+    # TestSync().run()
+    TestAsync().start()
