9a8135a824baea2d201640e4d2e5fe30ccb52647
timeyyy <timeyyy_da_man@hotmail.com>
2017-04-15 11:23:26 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'uncrumpled_kivygui',
-    version='0.1.0',
+    version='0.1.1',
     description = 'kivygui for uncrumpled',
     author='tmothy eichler',
     author_email='tim_eichler@hotmail.com',
